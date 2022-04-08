5a56168a8d2ead907fe5938d63432746ce084b09
AktamNosirov <94_aktam@mail.ru>
2022-04-08 09:57:42 +0500

    4-6

--- While06.py
+++ While06.py
@@ -7,4 +7,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    index=0
+    count=0
+    consonant=["a","e", "i"," o", "u"]
+    while index<len(s):
+        if not (s[index] in consonant or s[index].isdigit())  :
+            count+=1
+        index+=1
+    return count 
+print(main("a1s"))
